34541a347c9ee403502625a74f2262ab297c74fa
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2023-03-29 00:48:26 +0530

    ::ign:: => docs

--- dist/helpers/funcs.d.ts
+++ dist/helpers/funcs.d.ts
@@ -1,5 +1,10 @@
 import { Message } from "discord.js";
 export declare function err(desc: string, cmd?: string, warn?: boolean): string;
+/**Listen to button interactions
+ * @param users - array of user-ids who can click on button
+ * @param msg - the msg sent containing the buttons
+ * @param props - (?) listen to a specific button of an id only, define the max valid interactions and/or define the time(ms) interval for which the clicks will be valid
+ */
 export declare function buttonSignal(users: string[], msg: Message | undefined, props?: {
     customId?: string;
     max?: number;
--- dist/helpers/funcs.js
+++ dist/helpers/funcs.js
@@ -1,6 +1,11 @@
 export function err(desc, cmd, warn = false) {
     return `[${warn ? 'warn' : 'err'}][cmd: ${cmd}] ${desc}`;
 }
+/**Listen to button interactions
+ * @param users - array of user-ids who can click on button
+ * @param msg - the msg sent containing the buttons
+ * @param props - (?) listen to a specific button of an id only, define the max valid interactions and/or define the time(ms) interval for which the clicks will be valid
+ */
 export function buttonSignal(users, msg, props) {
     if (!msg)
         return;
